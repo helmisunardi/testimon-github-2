b072bd0a44b752f3ef34f796fe76cb48213f3e2e
helmisunardi <soenardijava212@gmail.com>
2022-06-24 08:43:13 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,11 @@
 <body>
  <h1>Hello World!</h1>
  <p>Curabitur maximus eu massa ut ornare. Fusce et rutrum libero, at ornare massa. Curabitur dapibus massa id dolor tristique malesuada. Vivamus pellentesque massa ut finibus sagittis. Ut eget tristique ex. Cras vulputate vel diam et ultrices. Nunc mollis metus eget tempor molestie</p>
+ <ul>
+   <li>satu</li>
+   <li>dua</li>
+   <li>tiga</li>
+ </ul>
 </body>
 
 </html>
